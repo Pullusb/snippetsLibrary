fadef6b211c58137387462de0c60831837041c11
Samuel Bernou <bernou.samuel@gmail.com>
2020-01-13 14:16:01 +0100

    new elapsed time with detail snippet

--- snippets/py/print_elapsed_time.py
+++ snippets/py/print_elapsed_time.py
@@ -1,5 +1,11 @@
-import time
-start_time = time.time()
+import time, datetime
+print('Starting', datetime.datetime.now())# print full current date
+start_time = time.time()# get start time
 
-elapsed_time = time.time() - start_time
+# Do stuff
+
+elapsed_time = time.time() - start_time# seconds
+full_time = str(datetime.timedelta(seconds=elapsed_time))# hh:mm:ss format
+
 print("elapsed time", elapsed_time)
+print(full_time)
