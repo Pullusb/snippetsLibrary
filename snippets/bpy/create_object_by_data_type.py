f0c6bacf2fd45f18c7a3f4c76fcf4d7c6de63e87
Pullusb <bernou.samuel@gmail.com>
2020-02-18 19:39:27 +0100

    add obj type

--- snippets/bpy/create_object_by_data_type.py
+++ snippets/bpy/create_object_by_data_type.py
@@ -4,12 +4,16 @@ import bpy
 ### 1 create object DATA and store object name.
 
 ## mesh
-ob_data = bpy.data.meshes.new("mesh_data_name")#name of the mesh data
+ob_data = bpy.data.meshes.new("mesh_data_name")
 ob_name = 'My_Mesh'
 
-## text
-ob_data = bpy.data.curves.new("text_data_name", "FONT")#('CURVE', 'SURFACE', 'FONT')
+## text (font is a curve objects)
+ob_data = bpy.data.curves.new("text_data_name", "FONT")# ('CURVE', 'SURFACE', 'FONT')
 ob_name = 'My_Text'
+
+## light
+ob_data = bpy.data.lights.new("light_data_name", "POINT")# ('POINT', 'SUN', 'SPOT', 'AREA')
+ob_name = 'My_Light'
 
 ## curve
 ob_data = bpy.data.curves.new("curve_data_name", "CURVE")
@@ -40,6 +44,8 @@ ob = bpy.data.objects.new(ob_name, ob_data)
 
 ## Active collection (master if no active collection)
 bpy.context.collection.objects.link(ob)
+# bpy.context.view_layer.active_layer_collection.collection.objects.link(ob) # active collection of current view layer
+
 
 ## Master (root) collection
 # bpy.context.scene.collection.objects.link(ob)
@@ -55,3 +61,10 @@ ob.location = bpy.context.scene.cursor.location
 
 ## or specific location
 # ob.location = (0,3,0)# ex : 3 units in depth, 2 up
+
+
+### 5 Select
+ob.select_set(True)
+
+## make active
+bpy.context.view_layer.objects.active = ob
