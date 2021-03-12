ca5d72c8813fea1d5be8308a159c39429c1a6816
Pullusb <bernou.samuel@gmail.com>
2021-03-12 14:10:14 +0100

    new snippet - get class definition

---
+++ snippets/py/helper_get_func_or_class_name_and_args.py
@@ -0,0 +1,13 @@
+## get a function/class name and it's argument within itself
+## e.g: print(helper())
+
+def helper(name=str:'') -> str:
+    '''Return name and arguments from calling obj as str
+    :name: - replace definition name by your own str
+    '''
+    import inspect
+    func = inspect.currentframe().f_back
+    name = name or f'def {func.f_code.co_name}'
+    args = inspect.getargvalues(func).locals
+    arguments = ', '.join([f'{k}={v}' for k,v in args.items()])
+    return( f'{name}({arguments})')
