1ece124cde60b1dfb3fae01be1d1ad31bd1b9bc1
Pullusb <bernou.samuel@gmail.com>
2021-03-12 14:25:18 +0100

    fix type hinting bug

--- snippets/py/helper_get_func_or_class_name_and_args.py
+++ snippets/py/helper_get_func_or_class_name_and_args.py
@@ -1,7 +1,7 @@
-## get a function/class name and it's argument within itself
+# get a function/class name and it's argument within itself
 ## e.g: print(helper())
 
-def helper(name=str:'') -> str:
+def helper(name: str = '') -> str:
     '''Return name and arguments from calling obj as str
     :name: - replace definition name by your own str
     '''
@@ -9,5 +9,5 @@ def helper(name=str:'') -> str:
     func = inspect.currentframe().f_back
     name = name or f'def {func.f_code.co_name}'
     args = inspect.getargvalues(func).locals
-    arguments = ', '.join([f'{k}={v}' for k,v in args.items()])
-    return( f'{name}({arguments})')
+    arguments = ', '.join([f'{k}={v}' for k, v in args.items()])
+    return(f'{name}({arguments})')
