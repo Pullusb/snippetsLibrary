9c12c641ad9b7cb50f3590660ae3228cc3689067
Pullusb <bernou.samuel@gmail.com>
2021-07-30 16:14:21 +0200

    snip driver basic correction

--- snippets/bpy/add_driver_basic.py
+++ snippets/bpy/add_driver_basic.py
@@ -1,8 +1,5 @@
-def add_driver(
-        source, target, prop, dataPath,
-        index = -1, negative = False, func = ''
-    ):
-    ''' Add driver to source prop (at index), driven by target dataPath '''
+def add_driver(source, target, prop, data_path, index=-1, invert=False, func=''):
+    ''' Add driver to source prop (at index), driven by target data_path '''
 
     if index != -1:
         d = source.driver_add( prop, index ).driver
@@ -12,7 +9,7 @@ def add_driver(
     v = d.variables.new()
     v.name                 = prop
     v.targets[0].id        = target
-    v.targets[0].data_path = dataPath
+    v.targets[0].data_path = data_path
 
     d.expression = func + "(" + v.name + ")" if func else v.name
-    d.expression = d.expression if not negative else "-1 * " + d.expression
+    d.expression = d.expression if not invert else "1 - " + d.expression
