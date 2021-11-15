cc653bc0a3696d5482375ce5c953572d0643dfea
Pullusb <bernou.samuel@gmail.com>
2021-11-15 14:03:18 +0100

    add math import

--- snippets/math/is_vector_close.py
+++ snippets/math/is_vector_close.py
@@ -2,4 +2,5 @@
 def is_vector_close(a, b, rel_tol=1e-03):
     '''compare Vector or sequence of value
     by default tolerance is set on 1e-03 (0.001)'''
+    import math
     return all([math.isclose(i, j, rel_tol=rel_tol) for i, j in zip(a,b)])
