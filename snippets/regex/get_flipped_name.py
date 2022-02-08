baf72c9cffade3007d24a6e6234399dd43b1d237
Pullusb <bernou.samuel@gmail.com>
2022-02-08 15:00:06 +0100

    fix regex error

--- snippets/regex/get_flipped_name.py
+++ snippets/regex/get_flipped_name.py
@@ -21,11 +21,12 @@ def get_flipped_name(name):
             sep, side, num = match.groups()
             return sep + sides[side] + (num or '')
 
-    start_reg = re.compile(r'^(l|r)([.-_])', flags=re.I)
+    start_reg = re.compile(r'^(l|r)([-_.])', flags=re.I)
 
     if start_reg.match(name):
         return start_reg.sub(lambda x: flip(x, True), name)
-    else:
-        return re.sub(r'([.-_])(l|r)(\.\d+)?$', flip, name, flags=re.I)
 
-print( get_flipped_name('sauce.r.001') )
+    else:
+        return re.sub(r'([-_.])(l|r)(\.\d+)?$', flip, name, flags=re.I)
+
+print( get_flipped_name('leg_R.001') )
