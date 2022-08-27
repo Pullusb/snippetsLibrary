f879ce660c6fa32d139ecf6c8ac1054558ae25a8
Pullusb <bernou.samuel@gmail.com>
2022-08-28 01:02:12 +0200

    fcurve massive removal

---
+++ snippets/bpy/remove_fcurves_by_data_path_name.py
@@ -0,0 +1,24 @@
+# remove all hide render fcurves on scene objects 
+import bpy
+
+# fcurve data_path to remove
+fcurve_path = ('hide_render', 'hide_viewport')
+
+## remove on all actions
+for ac in bpy.data.actions:
+    for fc in reversed(ac.fcurves):
+        if fc.data_path in fcurve_path:
+            print(f'removing hide_render from action: {ac.name}')
+
+## remove on scene objects only (restore visibility)
+for ob in bpy.context.scene.objects:
+    if not ob.animation_data or not ob.animation_data.action:
+        continue
+    ac = ob.animation_data.action
+    for fc in reversed(ac.fcurves):
+        if fc.data_path in fcurve_path:
+            print(f'removing hide_render, object: {ob.name} (action: {ac.name})')
+            ac.fcurves.remove(fc)
+
+            # turn visible
+            ob.hide_viewport = ob.hide_render = False
