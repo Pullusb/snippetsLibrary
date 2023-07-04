eefb1100c2366f826648c8fc5974bcdddc22e5c3
pullusb <bernou.samuel@gmail.com>
2023-07-04 11:38:43 +0200

    better recursive get collec children

--- snippets/bpy/get_all_childs_collections_recursive.py
+++ snippets/bpy/get_all_childs_collections_recursive.py
@@ -1,10 +1,15 @@
-def get_collection_childs_recursive(col, cols=[]):
-    '''return a list of all the sub-collections in passed col'''
-    # Start from fresh list, otherwise uncleared "cols" list is used on next call
+## Get all children collection recursively
+
+def get_collection_children_recursive(col, cols=None) -> list:
+    '''return a list of all the child collections
+    and their subcollections in the passed collection'''
+
     cols = cols or [] 
     for sub in col.children:
         if sub not in cols:
             cols.append(sub)
         if len(sub.children):
-            cols = get_collection_childs_recursive(sub, cols)
+            cols = get_collection_children_recursive(sub, cols)
     return cols
+
+# get_collection_children_recursive(bpy.context.scene.collection)
