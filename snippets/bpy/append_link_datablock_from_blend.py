25395df98e017c0511549972c41a4874398d4159
pullusb <bernou.samuel@gmail.com>
2023-09-20 19:17:16 +0200

    fix datablock loader

--- snippets/bpy/append_link_datablock_from_blend.py
+++ snippets/bpy/append_link_datablock_from_blend.py
@@ -1,6 +1,17 @@
-## generic load datablock from blend
-def load_datablocks(self, src, names, type, link=True):
-    with bpy.data.libraries.load(str(src), link=link) as (data_from, data_to):
-        setattr(data_to, type, names)
+## Generic load datablock from blend (append/link)
 
-        return getattr(data_to, type)
+def load_datablocks(src, type, names, link=True):
+    '''append/link datablock from a blend file
+    ex: load_datablocks('path/to/file.blend', 'node_groups', ['ng_A', 'ng_B'])
+    args:
+    src (str or Path): path to source blend file
+    type (str): datablock type (types found under bpy.data)
+    names (list or str): single name or list of names to append/link
+    link (bool): link if True else Append datablock
+    '''
+
+    if isinstance(names, str):
+        names = [names]
+    with bpy.data.libraries.load(str(src), link=link) as (data_from, data_to):
+        setattr(data_to, type, [item for item in getattr(data_from, type) if item in names])
+    return getattr(data_to, type)
