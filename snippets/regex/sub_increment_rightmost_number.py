7d7238548cb15b8cd5d153109957632cf0449bcb
pullusb <bernou.samuel@gmail.com>
2025-06-17 14:27:10 +0200

    add f-string version

--- snippets/regex/sub_increment_rightmost_number.py
+++ snippets/regex/sub_increment_rightmost_number.py
@@ -5,6 +5,10 @@ name = 'r2d2-V3'
 ## oneliner : increment rightmost number (keep padding)
 new = re.sub(r'(\d+)(?!.*\d)', lambda x: str(int(x.group(1))+1).zfill(len(x.group(1))), name)
 
+## oneliner using f-string
+new = re.sub(r'(\d+)(?!.*\d)', lambda x: f"{int(x.group(1))+1:0{len(x.group(1))}d}", name)
+
+
 ## with named function
 def increment(match):
     return str(int(match.group(1))+1).zfill(len(match.group(1)))
