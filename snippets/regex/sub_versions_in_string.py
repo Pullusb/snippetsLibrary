1a218d7ebefb53a247b1607479916478aff63115
Pullusb <bernou.samuel@gmail.com>
2026-09-02 11:17:59 +0200

    reliable version string update

---
+++ snippets/regex/sub_versions_in_string.py
@@ -0,0 +1,36 @@
+## strip version updater
+
+import re
+
+def update_version_string(string, version, version_pattern=None):
+    """Update string with passed version on all occurence
+
+    Args:
+        string (str): the string to update
+        version (str|int): version to apply (will get same padding as replaced string)
+        version_pattern (str, optional): regex pattern for version
+    """
+
+    ## version match: lowercase 'v' followed by 3 digit.
+    ## Anchors: 
+    ##   - before: only if there is a separator before "-_/\" OR string start (can happen in output nodes).
+    ##   - after : only with no subsequent digits (ex: v29400)
+    default_pattern = r'(?:(?<=[-_./\\ ])|^)v(\d{3})(?![0-9])'
+
+    ## Simpler anchoring (preventing only match with subsequent digit)
+    # default_pattern = r'v(\d{3})(?![0-9])'
+
+    version_pattern = version_pattern or default_pattern
+    re_version = re.compile(version_pattern)
+
+    def replace(match):
+        new = str(int(version)).zfill(len(match.group(1)))
+        offset = match.start()
+        start, end = match.span(1)
+        whole = match.group(0)
+        return whole[:start - offset] + new + whole[end - offset:]
+
+    return re_version.sub(replace, string)
+
+print(update_version_string('v005/test-v255. also update v008 (but not this one maxv200, nor this one v20000)', 1))
+# result                 >>> v001/test-v001. also update v001 (but not this one maxv200, nor this one v20000)
